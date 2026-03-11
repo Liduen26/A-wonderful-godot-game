bb063a9062ac1aca7a8a56254d7d1e17677cbe36
Snowky92 <amaurypinheiro@hotmail.fr>
2026-03-11 19:38:19 +0100

    add button sound

--- scripts/ground_button.gd
+++ scripts/ground_button.gd
@@ -2,10 +2,12 @@ extends Node3D
 
 @onready var be_activated_by_object_component: BeActivatedByObjectComponent = %BeActivatedByObjectComponent
 @onready var press_component: PressComponent = %PressComponent
+@onready var sfx_click = $sfx_click
 
 func _ready() -> void:
 	be_activated_by_object_component.status_changed.connect(update_status)
 
 func update_status(new_status: bool) -> void:
 	press_component.activated = new_status
+	sfx_click.play()
 	pass
